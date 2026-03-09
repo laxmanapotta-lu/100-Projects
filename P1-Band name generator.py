c66b0fb22bfa00f3615761b8a2deae7e3aba0245
laxmanapotta-lu <laxmanapotta@gmail.com>
2026-03-09 09:58:23 +0530

    Add files via upload

---
+++ P1-Band name generator.py
@@ -0,0 +1,9 @@
+# It is a simplBand Name generator 
+
+print("Welcome to Band Name Generator")
+
+city = input(" Enter the City name where you grew up : ")
+print(city)
+pet_name = input(" Enter  your Pet Name : ")
+print(pet_name)
+print(" Your Band name may be:" + " " + city + " " + pet_name)
